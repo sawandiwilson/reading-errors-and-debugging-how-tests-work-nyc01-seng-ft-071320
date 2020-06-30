131c9862ba670ca663d8c23d80a153b884b3e523
Sawandi Wilson <sawandiwilson@gmail.com>
2020-06-30 19:36:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,4 @@ first_number = 1.54430
 second_number = 9888
 sum = first_number + second_number
 difference = first_number - second_number
-mult = first_number * second_number
+product = first_number * second_number
